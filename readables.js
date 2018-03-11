a7887f94cb6b6bb4f113bf2379199fb2d7ce0b64
Nick Nance <nance.nick@gmail.com>
2018-03-11 10:02:06 -0700

    remove request library

--- readables.js
+++ readables.js
@@ -1,20 +1,18 @@
-const request = require('request')
+const { request } = require('https')
 const { readFile } = require('fs')
 const path = require('path')
 const { Readable } = require('stream')
 
-const buildRequestOptions = (gqlQuery) => {
-    return Promise.resolve({
+const buildRequestOptions = ({headers, body}) => Promise.resolve({
+    body,
+    options: {
         method: 'POST',
-        uri: 'https://api.github.com/graphql',
-        json: true,
-        headers: {
-            'User-Agent': 'nick-nance-ck',
-            'Authorization': `token ${process.env.TOKEN}`
-        },
-        body: { query: gqlQuery }
-    })
-}
+        host: 'api.github.com',
+        path: '/graphql',
+        port: 443,
+        headers
+    }
+})
 
 const loadQuery = (fileName) => {
     const fullPath = path.resolve(`./queries/${fileName}.gql`)
@@ -29,34 +27,63 @@ const loadQuery = (fileName) => {
     })
 }
 
-const makeRequest = (options) => {
-    return new Promise((resolve, reject) => {
-        request(options, (err, res) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(res.body)
-            }
+const makeRequest = ({options, body}) => new Promise((resolve, reject) => {
+    request(options, (res) => {
+        let result = ''
+        res.on('data', (chunk) => result += chunk)
+        res.on('end', () => {
+            console.log(result)
+            resolve(JSON.parse(result))
         })
-    })
+        res.on('err', (err) => reject(err))
+    }).write(body)
+})
+
+
+const initialRequest = (options) => () => {
+    return makeRequest(options).then((data) => ({
+        data,
+        nextStep: requestCompleted(options)
+    }))
 }
 
-const githubRequest = (fileName) => {
-    const createStream = (options) => new Readable({
+const requestCompleted = (options) => () => Promise.resolve({
+    data: null,
+    nextStep: null
+})
+
+const createStream = (options) => {
+    const result = new Readable({
         objectMode: true,
-        complete: false,
         read(opts) {
-            if (this.complete) {
-                this.push(null)
-            } else {
-                makeRequest(options).then(function (data) {
-                    this.complete = true
-                    this.push(data)
-                }.bind(this))
-            }
+            this.nextStep().then(({data, nextStep}) => {
+                this.nextStep = nextStep
+                this.push(data)
+            })
         }
     })
+    result.nextStep = initialRequest(options)
+    return result
+}
+
+const buildHeaders = ({body}) => Promise.resolve({
+    body,
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': body.length,
+        'User-Agent': `${process.env.USER}`,
+        'Authorization': `token ${process.env.TOKEN}`
+    }
+})
+
+const buildBody = (variables) => (query) => Promise.resolve({
+    body: JSON.stringify({query})
+})
+
+const githubRequest = (fileName) => {
     return loadQuery(fileName)
+            .then(buildBody({owner: 'hapijs', name: 'hapi'}))
+            .then(buildHeaders)
             .then(buildRequestOptions)
             .then(createStream)
 }
